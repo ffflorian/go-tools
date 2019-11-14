dfffe2ca28a31fb43eaa651b83810378448ca412
Florian Keller <github@floriankeller.de>
2019-11-14 11:41:05 +0100

    fix go get

--- gh-open/main.go
+++ gh-open/main.go
@@ -17,7 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
-import "./cmd"
+import "github.com/ffflorian/go-tools/gh-open/cmd"
 
 func main() {
 	cmd.Execute()
