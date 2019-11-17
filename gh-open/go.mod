6c6560f850c80f9317ac95ccd45974939524d937
Florian Keller <github@floriankeller.de>
2019-11-17 16:49:42 +0100

    Move everything into one file

--- gh-open/go.mod
+++ gh-open/go.mod
@@ -3,6 +3,8 @@ module github.com/ffflorian/go-tools/gh-open
 go 1.13
 
 require (
+	github.com/onsi/gomega v1.7.1 // indirect
 	github.com/skratchdot/open-golang v0.0.0-20190402232053-79abb63cd66e
 	github.com/spf13/cobra v0.0.5
+	github.com/tcnksm/go-gitconfig v0.1.2
 )
--- gh-open/main.go
+++ gh-open/main.go
@@ -17,8 +17,58 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
-import "github.com/ffflorian/go-tools/gh-open/cmd"
+import (
+	"fmt"
+	"github.com/skratchdot/open-golang/open"
+	"github.com/spf13/cobra"
+	"github.com/tcnksm/go-gitconfig"
+	"os"
+)
+
+var cfgFile string
+var justPrint bool
+var openBranch bool
+var printVersion bool
+var version string = "0.0.1"
+
+var rootCmd = &cobra.Command{
+	Use:   "gh-open",
+	Short: "Open a GitHub repository in your browser.",
+	Long:  "Open a GitHub repository in your browser.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("args", args)
+
+		if printVersion {
+			fmt.Println(version)
+			os.Exit(0)
+		}
+
+		fmt.Println(getFullURL())
+
+		open.Run("https://google.com/")
+	},
+}
 
 func main() {
-	cmd.Execute()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&justPrint, "print", "p", false, "just print the URL")
+	rootCmd.PersistentFlags().BoolVarP(&openBranch, "branch", "b", false, "open the branch tree (and not the PR)")
+	rootCmd.PersistentFlags().BoolVarP(&printVersion, "version", "v", false, "output the version number")
+}
+
+func getFullURL() string {
+	url, err := gitconfig.OriginURL()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return url
 }
