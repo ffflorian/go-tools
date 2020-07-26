e76c6cd4f9e12c47d9dede0281dace6250b325be
Florian Keller <git@ffflorian.de>
2020-07-26 10:12:29 +0200

    chore: Use new email address

--- simplelogger/simplelogger.go
+++ simplelogger/simplelogger.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 Florian Keller <github@floriankeller.de>
+Copyright © 2019 Florian Keller <git@ffflorian.de>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
